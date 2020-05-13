7f96a9b1ccc14e5f4b94259b5b67241194064e72
mirellyagv <30295175+mirellyagv@users.noreply.github.com>
2020-05-13 17:31:52 -0500

    boton

--- assets/js/visibilidad.js
+++ assets/js/visibilidad.js
@@ -1,3 +1,8 @@
 $("#carga1").on('change',function(){
-	alert(this.value);
+	var id = this.id;
+	if(this.value != ''){
+		$('#label_'+id).removeClass('btn-primary');
+		$('#label_'+id).addClass('btn-secondary');
+	}
+	// alert(this.value);
 });
