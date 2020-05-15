9ae110fb4d05c68047d74892b6d3da70a04a3049
jhonamg <orion01123581321@gmail.com>
2020-05-14 22:16:30 -0500

    primera rev del dia

--- assets/js/precios.js
+++ assets/js/precios.js
@@ -1,1 +1,7 @@
-$(".numero").number(true,2);
+// $(".numero").number(true,2);
+// function numerar ($this) {
+// 	if(this.value.length > this.maxLength) 
+// 	{
+// 		this.value = this.value.slice(0, this.maxLength);
+// 	}
+// }
